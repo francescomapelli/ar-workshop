858ece2a2a917f69381e1917fc6489c94cd422ed
Francesco Mapelli <francesco.mapelli@gmail.com>
2019-04-05 11:56:59 +0200

    magenta ball in the world

--- app/src/main/java/com/nytimes/android/ramonaharrison/MainActivity.kt
+++ app/src/main/java/com/nytimes/android/ramonaharrison/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.nytimes.android.ramonaharrison
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.ShapeFactory
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val galleryAdapter = GalleryAdapter()
     private lateinit var galleryRecyclerview: RecyclerView
+    private lateinit var arFragment: ArFragment
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        arFragment = fragment as ArFragment
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            Log.d("LOG", "TAP!")
+            val anchor = hitResult.createAnchor()
+            addSphere(Color.MAGENTA, anchor, 0.1f, 0.0f, 0.15f, 0.0f)
+        }
         galleryRecyclerview = findViewById<RecyclerView>(R.id.gallery_recyclerview).apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = galleryAdapter
@@ -47,5 +65,21 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun addNodeToScene(anchor: Anchor, renderable: Renderable) {
+        val anchorNode = AnchorNode(anchor)
+        val node = TransformableNode(arFragment.transformationSystem)
+        node.renderable = renderable
+        node.setParent(anchorNode)
+        arFragment.arSceneView.scene.addChild(anchorNode)
+    }
+
+    private fun addSphere(color: Int, anchor: Anchor, radius: Float, centerX : Float, centerY: Float, centerZ : Float) {
+        MaterialFactory.makeOpaqueWithColor(this, com.google.ar.sceneform.rendering.Color(color))
+            .thenAccept { material ->
+                val shape = ShapeFactory.makeSphere(radius, Vector3(centerX, centerY, centerZ), material)
+                addNodeToScene(anchor, shape)
+            }
     }
 }
